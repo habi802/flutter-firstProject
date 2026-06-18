a9f7d4a7b70eff40b078f2bbc0353076b8fda029
habi <shb8002@gmail.com>
2026-06-18 21:09:08 +0900

    [feat] 형 변환

---
+++ lib/ch4/section1/typeCasting.dart
@@ -0,0 +1,12 @@
+main() {
+  int n1 = 10;
+  double d1 = 10.0;
+
+  double d2 = n1.toDouble();
+  int n2 = d1.toInt();
+
+  String s1 = '10';
+
+  String s2 = n1.toString();
+  int n3 = int.parse(s1);
+}
