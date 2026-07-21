8c839a40bfd23c43cb884ce8d24c9aa51de0d6ae
habi <shb8002@gmail.com>
2026-07-21 20:22:19 +0900

    [refactor] 메소드 채널

---
+++ android/app/src/main/kotlin/com/example/first/ch22/section2/MethodChannel.kt
@@ -0,0 +1,44 @@
+package com.example.first.ch22.section2
+
+import android.util.Log
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.BasicMessageChannel
+import io.flutter.plugin.common.EventChannel
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.StringCodec
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MethodChannel : FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+
+        // 메소드 채널 테스트
+        val methodChannel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "myMethodChannel")
+        methodChannel.setMethodCallHandler { call, result ->
+            io.flutter.Log.d("flutter", call.toString())
+
+            if (call.method == "oneMethod") {
+                val map = call.arguments as Map<String, String>
+                io.flutter.Log.d("flutter", "${map.get("Username")}, " + "${map.get("Password")}")
+                result.success(mapOf("one" to 10, "two" to 20))
+
+                // 데이터 보내기
+                methodChannel.invokeMethod("twoMethod", "Hello from Android", object : MethodChannel.Result {
+                    override fun success(result: Any?) {
+                        io.flutter.Log.d("flutter", "${result as String}")
+                    }
+
+                    override fun error(errorCode: String, errorMessage: String?, errorDetails: Any?) {
+                    }
+
+                    override fun notImplemented() {
+
+                    }
+                })
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+}
