cb09fd298bd65aa493a7d57f5cb64bb11be58d2a
habi <shb8002@gmail.com>
2026-07-30 21:19:15 +0900

    [feat] firestore_storage

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,8 @@ plugins {
 
 android {
     namespace = "com.example.first"
-    compileSdk = flutter.compileSdkVersion
+    //compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     //ndkVersion = flutter.ndkVersion
     ndkVersion = "27.0.12077973"
 
@@ -28,10 +29,12 @@ android {
         applicationId = "com.example.first"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+//        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
