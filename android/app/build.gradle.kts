eb0b90ac17f3224de37e1eddc8e7b6e036485730
habi <shb8002@gmail.com>
2026-08-05 04:20:55 +0900

    [feat] firebase_messaging

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,6 +18,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -44,6 +45,10 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
 
 flutter {
